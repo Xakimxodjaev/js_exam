d793f866d94870efef42bbd44056c2bad07b6006
Xakimxodjaev <f53660668@gmail.com>
2025-09-06 15:53:50 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+// 1
+
+// let num = 5 % 2;
+
+// alert(num)
+
+
+// 2
+
+
+// let num = Math.floor(Math.random() * 10) + 1;
+// alert(num);
+
+// 3
+
+// let num = 12.510;
+// alert(Math.floor(num));
+
+
+// 4
+
+
+
+// function harfsoni(str) {
+//     return str.length;
+// }
+
+// alert(harfsoni("MARS IT SCHOOL"));
+
+// 5
+
+// const print = () => {
+//     for (let i = 0; i < 10; i++) {
+//         console.log("MARS IT SCHOOL");
+//     }
+// }
+
+// print();
+
+
+// 6
+
+
+
+// let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ]
+
+
+// let name = harflar[0] + harflar[10] + harflar[7]
+
+// alert(name);
+
+
+
+// 7
+
+
+// let yosh = +prompt("Yoshingizni kiriting")
+
+// if (yosh > 18) {
+//     alert("Siz balag'ot yoshiga yetgansiz")
+// }
+
+// else if (yosh == 18) {
+//     alert("balog'at yoshi muborak")
+// }
+
+// else {
+//     alert("Siz balag'ot yoshiga yetmagansiz")
+// }
+
+// console.log(yosh);
+
+
+
+// 8
+
+
+// let name = prompt("Ismingizni kiriting")
+// let nbarot = name.split('').reverse().join('')
+// console.log(nbarot);
+
+
+// 9
+
+
+// let name = prompt("Ismingizni kiriting");
+// let age = prompt("Yoshingizni kiriting");
+
+// let div = document.createElement("div");
+// div.textContent = "Ismingiz: " + name + ", Yoshingiz: " + age;
+// div.style.background = "green";
+// div.style.color = "darkblue";
+// div.style.padding = "15px";
+// div.style.margin = "15px";
+// div.style.fontSize = "18px";
+// div.style.borderRadius = "10px";
+// div.style.textAlign = "center";
+
+// document.body.appendChild(div);
+
+
+// 10
+
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < sonlar.length; i++) {
+//     if (sonlar[i] % 2 === 0) {
+//         console.log(sonlar[i]);
+//     }
+// }
+
